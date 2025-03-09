4eb75be77175e26b77381da6940c0b315bcc5791
Jussi Päivinen <jussi.paivinen@gmail.com>
2025-03-09 22:02:52 +0200

    Added actual extension files

---
+++ options.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const optionsContainer = document.getElementById('optionsContainer');
+	const numberOfOptions = 25
+
+    for (let i = 1; i <= numberOfOptions; i++) {
+        const optionTitle = document.createElement('label');
+        const paddedNumberString = `${i}`.padStart(2, '0');
+        optionTitle.textContent = `URL for shortcut #${paddedNumberString}:`;
+        optionTitle.className = 'optionTitle';
+
+        const optionInput = document.createElement('input');
+        optionInput.type = 'text';
+        optionInput.id = `urlShortcut${i}`;
+        optionInput.className = 'optionInput';
+
+        const optionRow = document.createElement('div');
+        optionRow.className = 'optionRow';
+        optionRow.appendChild(optionTitle);
+        optionRow.appendChild(optionInput);
+        optionsContainer.appendChild(optionRow);
+
+        // Save value on change
+        optionInput.addEventListener('input', function() {
+            const updatedOptionValue = {};
+            updatedOptionValue[`urlShortcut${i}`] = optionInput.value;
+            chrome.storage.sync.set(updatedOptionValue, function() {
+                console.log(`Value for shortcut #${paddedNumberString} saved`);
+            });
+        });
+    }
+
+    // Load previously saved values
+    chrome.storage.sync.get(null, function(previousValues) {
+        for (let i = 1; i <= numberOfOptions; i++) {
+            const optionInput = document.getElementById(`urlShortcut${i}`);
+            if (previousValues[`urlShortcut${i}`]) {
+                optionInput.value = previousValues[`urlShortcut${i}`];
+            }
+        }
+    });
+});
---
+++ serviceWorker.js
@@ -0,0 +1,91 @@
+chrome.commands.onCommand.addListener(function(command) {
+	switch (command) {
+		case "Open URL shortcut #01":
+            openUrl('urlShortcut1');
+            break;
+        case "Open URL shortcut #02":
+            openUrl('urlShortcut2');
+            break;
+        case "Open URL shortcut #03":
+            openUrl('urlShortcut3');
+            break;
+        case "Open URL shortcut #04":
+            openUrl('urlShortcut4');
+            break;
+        case "Open URL shortcut #05":
+            openUrl('urlShortcut5');
+            break;
+        case "Open URL shortcut #06":
+            openUrl('urlShortcut6');
+            break;
+        case "Open URL shortcut #07":
+            openUrl('urlShortcut7');
+            break;
+        case "Open URL shortcut #08":
+            openUrl('urlShortcut8');
+            break;
+        case "Open URL shortcut #09":
+            openUrl('urlShortcut9');
+            break;
+        case "Open URL shortcut #10":
+            openUrl('urlShortcut10');
+            break;
+        case "Open URL shortcut #11":
+            openUrl('urlShortcut11');
+            break;
+        case "Open URL shortcut #12":
+            openUrl('urlShortcut12');
+            break;
+        case "Open URL shortcut #13":
+            openUrl('urlShortcut13');
+            break;
+        case "Open URL shortcut #14":
+            openUrl('urlShortcut14');
+            break;
+        case "Open URL shortcut #15":
+            openUrl('urlShortcut15');
+            break;
+        case "Open URL shortcut #16":
+            openUrl('urlShortcut16');
+            break;
+        case "Open URL shortcut #17":
+            openUrl('urlShortcut17');
+            break;
+        case "Open URL shortcut #18":
+            openUrl('urlShortcut18');
+            break;
+        case "Open URL shortcut #19":
+            openUrl('urlShortcut19');
+            break;
+        case "Open URL shortcut #20":
+            openUrl('urlShortcut20');
+            break;
+        case "Open URL shortcut #21":
+            openUrl('urlShortcut21');
+            break;
+        case "Open URL shortcut #22":
+            openUrl('urlShortcut22');
+            break;
+        case "Open URL shortcut #23":
+            openUrl('urlShortcut23');
+            break;
+        case "Open URL shortcut #24":
+            openUrl('urlShortcut24');
+            break;
+        case "Open URL shortcut #25":
+            openUrl('urlShortcut25');
+            break;
+	}
+});
+
+function openUrl(key) {
+    chrome.storage.sync.get(key, function(result) {
+        const urlString = result[key];
+        try {
+            const url = new URL(urlString);
+            chrome.tabs.create({ url: url.href });
+        } catch (error) {
+            console.warning('Quick URL Shortcuts tried opening an invalid URL:', error);
+        }
+    });
+}
